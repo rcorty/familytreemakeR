5ddaddc0029e020c54107a5deb093768a70640e4
Robert Corty <rcorty@gmail.com>
2022-02-20 20:44:45 -0600

    create simple tests for new_family_tree()

--- tests/testthat/test-core-functions.R
+++ tests/testthat/test-core-functions.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+test_that(
+	desc = 'can make new family tree',
+	code =
+		{
+			expect_silent(new_family_tree())
+			expect_silent(new_family_tree(tree_name = 'test tree'))
+		}
+)
