3b272e08ceb83cba2560accacf662a3872f68064
Robert Corty <rcorty@gmail.com>
2022-02-20 22:42:48 -0600

    a tree is now an igraph rather than a list, improved definition of 'new tree' (must have a person in it) and function to make this new type of tree

--- R/core-functions.R
+++ R/core-functions.R
@@ -1,13 +1,33 @@
 #' @title Core Functions
 #'
-#' @param tree_name a name for the tree to be created,
-#' e.g. 'The Wallace Family'
+#' @param fn first name
+#' @param ln last name
+#' @param sex sex
+#' @param nn nick name or alternate name
+#' @param yob year of birth
+#' @param note other notes about a person
 #'
-#' @return a new tree
+#' @return a new family tree with one person
+#'
 #' @export
-#'
 #' @examples
-#' new_family_tree(tree_name = 'The Wallace Family')
-new_family_tree <- function(tree_name = 'my new tree') {
-	return(list(name = tree_name))
+#' new_family_tree(fn = 'Abraham', ln = 'Lincoln')
+#' new_family_tree(fn = 'Abraham', ln = 'Lincoln', nn = 'Honest Abe', yob = 1809)
+#'
+new_family_tree <- function(fn = NULL,
+														ln = NULL,
+														sex = NULL,
+														nn = NULL,
+														yob = NULL,
+														note = NULL) {
+	if (any(is.null(fn), is.null(ln))) {
+		stop('Must specify first name and last name of first person added to family tree.')
+	}
+	igraph::make_empty_graph() %>%
+		igraph::add_vertices(nv = 1, fn = fn, ln = ln,
+												 sex = sex, nn = nn,
+												 yob = yob, note = note) %>%
+		magrittr::set_class(c('family_tree', 'dag', 'igraph'))
 }
+
+
--- tests/testthat/test-core-functions.R
+++ tests/testthat/test-core-functions.R
@@ -2,7 +2,41 @@ test_that(
 	desc = 'can make new family tree',
 	code =
 		{
-			expect_silent(new_family_tree())
-			expect_silent(new_family_tree(tree_name = 'test tree'))
+			# not enough input
+			expect_error(new_family_tree())
+
+			# minimal sufficient input
+			tree1 <- new_family_tree(fn = 'Abraham', ln = 'Lincoln')
+			expect_s3_class(
+				object = tree1,
+				class = c('family_tree', 'dag', 'igraph'),
+				exact = TRUE
+			)
+			expect_identical(
+				object = igraph::as_data_frame(x = tree1, what = 'vertices'),
+				expected = data.frame(fn = 'Abraham', ln = 'Lincoln')
+			)
+			expect_identical(
+				object = length(igraph::E(graph = tree1)),
+				expected = 0L
+			)
+
+			# more input
+			tree2 <-  new_family_tree(fn = 'Abraham', ln = 'Lincoln',
+																nn = 'Honest Abe', yob = 1809)
+			expect_s3_class(
+				object = tree2,
+				class = c('family_tree', 'dag', 'igraph'),
+				exact = TRUE
+			)
+			expect_identical(
+				object = igraph::as_data_frame(x = tree2, what = 'vertices'),
+				expected = data.frame(fn = 'Abraham', ln = 'Lincoln',
+															nn = 'Honest Abe', yob = 1809)
+			)
+			expect_identical(
+				object = length(igraph::E(graph = tree1)),
+				expected = 0L
+			)
 		}
 )
