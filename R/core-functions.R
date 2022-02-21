d2af7f4bd6ef34fe7b303090f925be8141a9a956
Robert Corty <rcorty@gmail.com>
2022-02-20 20:37:58 -0600

    document new_family_tree

--- R/core-functions.R
+++ R/core-functions.R
@@ -1,3 +1,13 @@
+#' @title Core Functions
+#'
+#' @param tree_name a name for the tree to be created,
+#' e.g. 'The Wallace Family'
+#'
+#' @return a new tree
+#' @export
+#'
+#' @examples
+#' new_family_tree(tree_name = 'The Wallace Family')
 new_family_tree <- function(tree_name = 'my new tree') {
 	return(list(name = tree_name))
 }
