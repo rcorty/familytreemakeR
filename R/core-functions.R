29fdefad1c6ff8094a09e341c93359fae3e56875
Robert Corty <rcorty@gmail.com>
2022-02-20 23:33:10 -0600

    improved names and fixed a bug in new_family_tree that caused parameters other than first name and last name not to be added to first vertex. also added 'last_added' param on the family tree overall to indicate which person last added

--- R/core-functions.R
+++ R/core-functions.R
@@ -1,33 +1,43 @@
 #' @title Core Functions
 #'
-#' @param fn first name
-#' @param ln last name
+#' @param first_name first name
+#' @param last_name last name
 #' @param sex sex
-#' @param nn nick name or alternate name
-#' @param yob year of birth
+#' @param nick_name nick name or alternate name
+#' @param year_of_birth year of birth
 #' @param note other notes about a person
 #'
 #' @return a new family tree with one person
 #'
 #' @export
 #' @examples
-#' new_family_tree(fn = 'Abraham', ln = 'Lincoln')
-#' new_family_tree(fn = 'Abraham', ln = 'Lincoln', nn = 'Honest Abe', yob = 1809)
+#' new_family_tree(first_name = 'Abraham', last_name = 'Lincoln')
+#' new_family_tree(first_name = 'Abraham', last_name = 'Lincoln',
+#'                 nick_name = 'Honest Abe', year_of_birth = 1809)
 #'
-new_family_tree <- function(fn = NULL,
-														ln = NULL,
-														sex = NULL,
-														nn = NULL,
-														yob = NULL,
-														note = NULL) {
-	if (any(is.null(fn), is.null(ln))) {
+new_family_tree <- function(
+	first_name = NULL,
+	last_name = NULL,
+	sex = NULL,
+	nick_name = NULL,
+	year_of_birth = NULL,
+	note = NULL
+) {
+	if (any(is.null(first_name), is.null(last_name))) {
 		stop('Must specify first name and last name of first person added to family tree.')
 	}
 	igraph::make_empty_graph() %>%
-		igraph::add_vertices(nv = 1, fn = fn, ln = ln,
-												 sex = sex, nn = nn,
-												 yob = yob, note = note) %>%
-		magrittr::set_class(c('family_tree', 'dag', 'igraph'))
+		igraph::add_vertices(
+			nv = 1,
+			first_name = first_name,
+			last_name = last_name,
+			sex = sex,
+			nick_name = nick_name,
+			year_of_birth = year_of_birth,
+			note = note
+		) %>%
+		magrittr::set_class(c('family_tree', 'dag', 'igraph')) %>%
+		magrittr::set_attr(which = 'last_added', value = 1L)
 }
 
 
--- tests/testthat/test-core-functions.R
+++ tests/testthat/test-core-functions.R
@@ -6,7 +6,7 @@ test_that(
 			expect_error(new_family_tree())
 
 			# minimal sufficient input
-			tree1 <- new_family_tree(fn = 'Abraham', ln = 'Lincoln')
+			tree1 <- new_family_tree(first_name = 'Abraham', last_name = 'Lincoln')
 			expect_s3_class(
 				object = tree1,
 				class = c('family_tree', 'dag', 'igraph'),
@@ -14,7 +14,7 @@ test_that(
 			)
 			expect_identical(
 				object = igraph::as_data_frame(x = tree1, what = 'vertices'),
-				expected = data.frame(fn = 'Abraham', ln = 'Lincoln')
+				expected = data.frame(first_name = 'Abraham', last_name = 'Lincoln')
 			)
 			expect_identical(
 				object = length(igraph::E(graph = tree1)),
@@ -22,8 +22,8 @@ test_that(
 			)
 
 			# more input
-			tree2 <-  new_family_tree(fn = 'Abraham', ln = 'Lincoln',
-																nn = 'Honest Abe', yob = 1809)
+			tree2 <-  new_family_tree(first_name = 'Abraham', last_name = 'Lincoln',
+																nick_name = 'Honest Abe', year_of_birth = 1809)
 			expect_s3_class(
 				object = tree2,
 				class = c('family_tree', 'dag', 'igraph'),
@@ -31,8 +31,8 @@ test_that(
 			)
 			expect_identical(
 				object = igraph::as_data_frame(x = tree2, what = 'vertices'),
-				expected = data.frame(fn = 'Abraham', ln = 'Lincoln',
-															nn = 'Honest Abe', yob = 1809)
+				expected = data.frame(first_name = 'Abraham', last_name = 'Lincoln',
+															nick_name = 'Honest Abe', year_of_birth = 1809)
 			)
 			expect_identical(
 				object = length(igraph::E(graph = tree1)),
